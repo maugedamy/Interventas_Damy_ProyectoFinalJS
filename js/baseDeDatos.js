0d210aa130a7e424e45c5e5934c68b0f9e3be010
mauged <m.eugenia.damy@gmail.com>
2022-07-13 21:30:47 -0300

    fetch

--- js/baseDeDatos.js
+++ js/baseDeDatos.js
@@ -1,26 +1,8 @@
-celulares.push(new Celular(crearID(), "Iphone", "Iphone 12", 250000, "./assets/iphone12.jpg"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Iphone", "Iphone 13 Pro", 260000, "./assets/iphone13pro.jpg"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Iphone", "Iphone SE", 185000, "./assets/iphoneSe.webp"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Samsung", "Samsung Galaxy A12", 60000, "./assets/SamsungGalaxyA12.png"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Samsung", "Samsung Galaxy S20", 105000, "./assets/SamsungGalaxyS20.jpg"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Samsung", "Samsung Galaxy S22", 170000, "./assets/samsungGalaxyS22.png"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Motorola", "Motorola Edge 30", 90000, "./assets/MotorolaEdge30.png"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Motorola", "Motorola Edge 30 Pro", 160000, "./assets/MotorolaEdge30Pro.png"));
-cargarCelulares(celulares[id - 1]);
-
-celulares.push(new Celular(crearID(), "Motorola", "Motorola Moto G22", 38000, "./assets/MotorolaMotoG22.jpg"));
-cargarCelulares(celulares[id - 1]);
+fetch('./json/lista.json')
+    .then((response) => response.json())
+    .then((respCelulares) => {
+        respCelulares.celulares.forEach((respCelular) => {
+            celulares.push(new Celular(crearID(), respCelular.marca, respCelular.modelo, respCelular.precio, respCelular.imagen));
+            cargarCelulares(celulares[id - 1])
+        });
+    })
